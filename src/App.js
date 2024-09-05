214bcd208bc560d442b11bf44f99b483df407f13
AsamPrabhakarReddy <94506736+AsamPrabhakarReddy@users.noreply.github.com>
2024-09-05 12:50:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> deploying with circle ci and aws s3 integration</h1>
+      <h1> deploying with circle ci and aws s3 integration, this is commit by github changes</h1>
+      <p> To check wheather automatically changes are happening or not </p>
     </div>
   );
 }
